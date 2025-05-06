247e5131005caa8f8e7e28dbbb2b640f351f6bbf
ranvir <rjkind01@gmail.com>
2025-05-06 03:11:55 -0700

    Fix: wrap custom nav icons in buttons to match expected button props

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,16 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        PreviousMonthButton: (props) => <ChevronLeft {...props} className="h-4 w-4" />,
-        NextMonthButton: (props) => <ChevronRight {...props} className="h-4 w-4" />
+        PreviousMonthButton: (props) => (
+          <button {...props}>
+            <ChevronLeft className="h-4 w-4" />
+          </button>
+        ),
+        NextMonthButton: (props) => (
+          <button {...props}>
+            <ChevronRight className="h-4 w-4" />
+          </button>
+        )
       }}
       {...props}
     />
