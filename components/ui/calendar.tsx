99b45c830d257ff443e770e32b67a9d3fb4d958b
ranvir <rjkind01@gmail.com>
2025-05-06 03:04:59 -0700

    Fix calendar component custom components keys to match types

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />
+        PreviousMonthButton: (props) => <ChevronLeft {...props} className="h-4 w-4" />,
+        NextMonthButton: (props) => <ChevronRight {...props} className="h-4 w-4" />
       }}
       {...props}
     />
